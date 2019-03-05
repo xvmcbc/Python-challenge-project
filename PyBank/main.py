2d49299cf76660ede4f986fb8e7c042d722cb7cb
Xavier Vazquez M <xvmc.bc@gmail.com>
2019-03-05 12:14:21 -0600

    Update main.py
    Code for the first exercise of the homework, working with Budget_data.csv

---
+++ PyBank/main.py
@@ -0,0 +1,61 @@
+#Libraries to read CSVs and processing path routes
+import os
+import csv
+
+#Path of the file
+csvpath = "budget_data.csv"
+
+count_months = 0
+net_amount = 0
+p_ant = 0
+tot_change = 0
+record = []
+date = []
+
+#CSV file processing...
+with open(csvpath, newline='') as csvfile:
+
+   # CSV reader specifies delimiter and variable that holds contents
+   csvreader = csv.reader(csvfile, delimiter=',')
+
+  # Read the header and skip it
+   csv_header = next(csvreader,None)
+   
+ # 1-Analyze the total number of months included in the dataset 
+ # 2-The net total amount of "Profit/Losses" over the entire period
+ # 3-The average of the changes in "Profit/Losses" over the entire period
+ # 4-The greatest increase in profits (date and amount) over the entire period
+ # 5-The greatest decrease in losses (date and amount) over the entire period
+   for row in csvreader:
+
+    if row[0] is not None:
+      count_months += 1
+      net_amount += int(row[1])
+    
+    if count_months >= 2:
+      date.append(row[0])
+      record.append((int(row[1])) - p_ant)
+
+    p_ant = int(row[1])
+
+#Total of changes / number of changes
+average_changes = round(sum(record)/(len(record)),2)
+
+#Max increase and decrease in profits
+max_increase = max(record)
+max_decrease = min(record)
+date_increase = date[record.index(max_increase)]
+date_decrease = date[record.index(max_decrease)]
+
+#Printing the results  
+print("Number of months : " + str(count_months))  
+print("Net Total amount : $" + str(net_amount))   
+print("Average changes  : $" + str(average_changes))    
+print("Greatest increase in profit: " + str(date_increase) + " " + str(max_increase))
+print("Greatest increase in profit: " + str(date_decrease) + " " + str(max_decrease))
+
+      
+      
+   
+
+
